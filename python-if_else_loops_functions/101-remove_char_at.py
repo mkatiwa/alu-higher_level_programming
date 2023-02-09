d72dff347959e5ee3e8457a68a45cb23bd8ad7bc
mkatiwa <m.katiwa@alustudent.com>
2023-02-09 15:48:00 -0800

    mawia

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+# Author - bamidele Adefolaju
+
 def remove_char_at(str, n):
     if n < 0:
         return (str)
     return (str[:n] + str[n+1:])
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
