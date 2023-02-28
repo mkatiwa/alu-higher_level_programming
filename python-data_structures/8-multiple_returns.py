7765d4223f618a08f8107ec0ed999e625a9baea1
mkatiwa <m.katiwa>
2023-02-28 03:26:32 -0800

    yas

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 8-multiple_returns.py
+
+
+def multiple_returns(sentence):
+    """Returns the length of the character and its first character."""
+    if sentence =="":
+        return (0, none)
+    return (len(sentence), sentence[0])
