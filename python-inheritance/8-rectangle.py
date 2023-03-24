fdd1f34299e98329e9318649e5f2e64d36d381d0
mkatiwa <m.katiwa@alustudent.com>
2023-03-24 07:04:54 -0700

    ghjdbc

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,7 +7,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """class rectangle inherited from BaseGeometry"""
+
     def __init__(self, width, height):
+        """Initialize data."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
