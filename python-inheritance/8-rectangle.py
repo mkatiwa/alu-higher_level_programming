9f9267b566000226597afef84bc08ef61ecfde2d
mkatiwa <m.katiwa@alustudent.com>
2023-03-24 07:18:17 -0700

    the best

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Inherits from BaseGeometry"""
+"""Inherits BaseGeometry class documented now"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class rectangle inherited from BaseGeometry"""
+    """Class Rectangle"""
 
     def __init__(self, width, height):
         """Initialize data."""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
+
