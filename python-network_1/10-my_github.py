e845040682574f8b2320594bad4101dcceb64711
mkatiwa <m.katiwa@alustudent.com>
2023-04-07 14:39:17 +0200

    my github

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A python script that uses the GitHub API to display your id"""
+from requests import get, auth
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
