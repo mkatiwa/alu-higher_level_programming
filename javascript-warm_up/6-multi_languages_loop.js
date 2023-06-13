44ba98a5b6f1f5b30c7069ccee86448705083e22
mkatiwa <m.katiwa@alustudent.com>
2023-06-13 14:58:52 +0300

    loops to language

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const strs = ['C is fun', 'python is cool', 'JavaScript is amazing'];
+const strs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const str of strs) {
 	console.log(str);
